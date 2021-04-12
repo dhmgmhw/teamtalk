f7c0a027a4509374a82e71eea8cab7d39b76dfd0
NOhhwasung <sinical909@naver.com>
2021-04-12 21:04:07 +0900

    chore : LogBox 추가

--- App.jsx
+++ App.jsx
@@ -1,13 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import StackNavigator from './navigation/StackNavigator';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import StackNavigator from "./navigation/StackNavigator";
+import { LogBox } from "react-native";
 
 export default function App() {
+  LogBox.ignoreLogs(["Warning: ..."]);
   return (
     <NavigationContainer>
-      <StatusBar style='black' />
+      <StatusBar style="black" />
       <StackNavigator />
     </NavigationContainer>
   );
@@ -16,8 +18,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
