bdbd18c242d0516493d6ec3e8c6d246ff9869e45
NOhhwasung <sinical909@naver.com>
2021-04-12 11:42:23 +0900

    feat:로그인페이지 추가

---
+++ components/ButtonItem.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { StyleSheet, TouchableOpacity } from "react-native";
+import { Text } from "native-base";
+
+export default function ButtonItem({ title, navigation, page }) {
+  return (
+    <TouchableOpacity
+      style={styles.button}
+      onPress={() => navigation.navigate(page)}>
+      <Text style={styles.text}>{title}</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: "#F2181C",
+    width: 265,
+    height: 40,
+    borderRadius: 15,
+    alignSelf: "center",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    color: "white",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+});
--- pages/login/LoginPage.jsx
+++ pages/login/LoginPage.jsx
@@ -1,12 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
+import { getStatusBarHeight } from "react-native-status-bar-height";
 
-export default function LoginPage() {
+const talk = require("../../assets/teamtalk.png");
+
+import ButtonItem from "../../components/ButtonItem";
+import TextItem from "../../components/TextItem";
+import InputItem from "../../components/InputItem";
+
+export default function LoginPage({ navigation }) {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
+  useEffect(() => {
+    navigation.addListener("beforeRemove", (e) => {
+      e.preventDefault();
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up LoginPage.js to start working on your app!</Text>
-      <StatusBar style='auto' />
+      <StatusBar style="auto" />
+      <Image source={talk} resizeMode="cover" style={{ marginBottom: 30 }} />
+      <InputItem hint={"이름"} value={name} setFunc={setName} />
+      <InputItem
+        hint={"비밀번호"}
+        type={"password"}
+        value={password}
+        setFunc={setPassword}
+      />
+      <ButtonItem title={"Log In"} navigation={navigation} page={"MainPage"} />
+
+      <View style={{ marginTop: 100 }}>
+        <TextItem
+          title={"Sign up"}
+          navigation={navigation}
+          page={"SignUpPage"}
+        />
+      </View>
     </View>
   );
 }
@@ -14,8 +46,9 @@ export default function LoginPage() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: getStatusBarHeight(),
+    backgroundColor: "#202540",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
