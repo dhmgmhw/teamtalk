f160311f7141d1e2b33dd46a49e35c91d1d54f9f
dhmgmhw <dhmgmhw@gmail.com>
2021-04-14 12:07:57 +0900

    feat: 보드페이지 각 슬라이드 구현

--- pages/board/BoardPage.jsx
+++ pages/board/BoardPage.jsx
@@ -1,15 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import {
   StyleSheet,
   Text,
   ScrollView,
   Dimensions,
-  TouchableOpacity,
+  TextInput,
+  Pressable,
 } from 'react-native';
 import BoardHeaderComponent from '../../components/board/BoardHeaderComponent';
-import { Container, View, Button, Icon } from 'native-base';
+import { Container, View } from 'native-base';
 import Swiper from 'react-native-swiper-hooks';
 
 const diviceWidth = Dimensions.get('window').width;
@@ -18,38 +19,69 @@ const diviceHeight = Dimensions.get('window').height;
 export default function BoardPage({ navigation, route }) {
   const data = route.params;
   const title = data.title;
+  const pin = data.pin;
+
+  const [text, onChangeText] = useState();
+  const [inputVisible, SetInputVisible] = useState(false);
+
   useEffect(() => {
-    console.log(data);
+    console.log(pin);
   }, []);
 
   return (
-    <Container>
+    <Container
+      style={{
+        backgroundColor: '#EBEBEB',
+      }}>
       <StatusBar style='light' />
       <BoardHeaderComponent navigation={navigation} title={title} />
       <View>
         <Swiper
           height={diviceHeight}
+          showPagination={true}
+          paginationSelectedColor={'black'}
           autoplay={false}
           loop={false}
-          showPagination={true}
           direction={'row'}
           style={styles.pinCard}>
-          <ScrollView
+          <View
             style={{
               width: diviceWidth,
-              backgroundColor: 'lightgrey',
             }}>
-            <Text>오하요</Text>
-          </ScrollView>
+            <View style={styles.addBox}>
+              {inputVisible ? (
+                <TextInput
+                  style={styles.input}
+                  placeholder='새로운 핀 이름'
+                  onChangeText={onChangeText}
+                  value={text}
+                />
+              ) : (
+                <></>
+              )}
+              <Pressable
+                style={styles.addBtn}
+                onPress={() => {
+                  {
+                    inputVisible
+                      ? SetInputVisible(false)
+                      : SetInputVisible(true);
+                  }
+                  console.log('add!');
+                }}>
+                <Text
+                  style={{
+                    fontSize: 18,
+                    fontWeight: '500',
+                    color: 'white',
+                    textAlign: 'center',
+                  }}>
+                  Add...
+                </Text>
+              </Pressable>
+            </View>
+          </View>
         </Swiper>
-
-        <Button
-          style={styles.addBtn}
-          onPress={() => {
-            console.log('Add Pin');
-          }}>
-          <Icon name='add' style={{ color: 'white', fontSize: 40, right: 4 }} />
-        </Button>
       </View>
     </Container>
   );
@@ -60,19 +92,24 @@ const styles = StyleSheet.create({
     padding: 10,
   },
   addBtn: {
-    position: 'absolute',
-    height: 60,
-    width: 60,
-    borderRadius: 100,
-    backgroundColor: '#F2181C',
-    shadowColor: 'black',
-    shadowOffset: {
-      width: 1,
-      height: 3,
-    },
-    shadowOpacity: 0.5,
-    shadowRadius: 3,
-    top: diviceHeight * 0.75,
-    right: 50,
+    alignSelf: 'center',
+    width: 100,
+  },
+  addBox: {
+    width: '85%',
+    backgroundColor: '#202540',
+    alignSelf: 'center',
+    borderRadius: 10,
+    marginTop: 20,
+    padding: 10,
+  },
+  input: {
+    backgroundColor: '#EBEBEB',
+    width: '100%',
+    height: 40,
+    paddingLeft: 10,
+    alignSelf: 'center',
+    borderRadius: 10,
+    marginBottom: 10,
   },
 });
