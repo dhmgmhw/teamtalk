2f81c9402fb42f126083165445f766370f9fbeb6
dhmgmhw <dhmgmhw@gmail.com>
2021-04-14 09:36:40 +0900

    style: header & boardheader margin

--- navigation/StackNavigator.jsx
+++ navigation/StackNavigator.jsx
@@ -1,11 +1,11 @@
-import React from "react";
-import { createStackNavigator } from "@react-navigation/stack";
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
 
-import LoginPage from "../pages/login/LoginPage";
-import SignUpPage from "../pages/login/SignUpPage";
-import MainPage from "../pages/main/MainPage";
-import BoardPage from "../pages/board/BoardPage";
-import PinDetail from "../pages/board/PinDetail";
+import LoginPage from '../pages/login/LoginPage';
+import SignUpPage from '../pages/login/SignUpPage';
+import MainPage from '../pages/main/MainPage';
+import BoardPage from '../pages/board/BoardPage';
+import PinDetail from '../pages/board/PinDetail';
 
 const Stack = createStackNavigator();
 
@@ -15,11 +15,11 @@ const StackNavigator = () => {
       screenOptions={{
         headerShown: false,
       }}>
-      <Stack.Screen name="LoginPage" component={LoginPage} />
-      <Stack.Screen name="SignUpPage" component={SignUpPage} />
-      <Stack.Screen name="MainPage" component={MainPage} />
-      <Stack.Screen name="BoardPage" component={BoardPage} />
-      <Stack.Screen name="PinDetail" component={PinDetail} />
+      {/* <Stack.Screen name="LoginPage" component={LoginPage} />
+      <Stack.Screen name="SignUpPage" component={SignUpPage} /> */}
+      <Stack.Screen name='MainPage' component={MainPage} />
+      <Stack.Screen name='BoardPage' component={BoardPage} />
+      <Stack.Screen name='PinDetail' component={PinDetail} />
     </Stack.Navigator>
   );
 };
