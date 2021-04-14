cdb6382b3f3c1cd0e4703f564b4b467e06377840
dhmgmhw <dhmgmhw@gmail.com>
2021-04-14 12:06:15 +0900

    style: headers

--- components/board/BoardHeaderComponent.jsx
+++ components/board/BoardHeaderComponent.jsx
@@ -1,55 +1,40 @@
 import React from 'react';
-import { StyleSheet, Dimensions, Image, Text } from 'react-native';
-import { Header, Left, Body, Right, Button, Icon } from 'native-base';
-const diviceWidth = Dimensions.get('window').width;
+import { StyleSheet, Text } from 'react-native';
+import { Icon } from 'native-base';
+import { Header } from 'react-native-elements';
 
 export default function BoardHeaderComponent({ navigation, title }) {
   return (
-    <Header style={styles.body}>
-      <Left>
-        <Button
-          transparent
+    <Header
+      backgroundColor={'#202540'}
+      leftComponent={
+        <Icon
+          name='chevron-back-sharp'
+          style={{ color: 'white', fontSize: 35 }}
           onPress={() => {
             navigation.goBack();
-          }}>
-          <Icon
-            name='chevron-back-sharp'
-            style={{ color: 'white', fontSize: 35 }}
-          />
-        </Button>
-      </Left>
-      <Body>
-        <Text ellipsizeMode={'tail'} numberOfLines={1} style={styles.groupName}>
-          {title}
-        </Text>
-      </Body>
-      <Right>
-        <Button
-          transparent
+          }}
+        />
+      }
+      centerComponent={<Text style={styles.groupName}>{title}</Text>}
+      rightComponent={
+        <Icon
           onPress={() => {
             console.log('참가인원');
-          }}>
-          <Icon
-            name='people-sharp'
-            style={{ color: 'white', fontSize: 30, top: 2 }}
-          />
-        </Button>
-      </Right>
-    </Header>
+          }}
+          name='people-sharp'
+          style={{ color: 'white', fontSize: 30, top: 2 }}
+        />
+      }
+    />
   );
 }
 const styles = StyleSheet.create({
-  body: {
-    paddingBottom: 10,
-    backgroundColor: '#202540',
-    height: 40,
-  },
   groupName: {
     fontSize: 15,
     color: 'white',
     fontWeight: '600',
-    top: 3,
+    top: 10,
     alignSelf: 'center',
-    marginLeft: 40,
   },
 });
--- components/main/HeaderComponent.jsx
+++ components/main/HeaderComponent.jsx
@@ -1,31 +1,28 @@
 import React from 'react';
-import { StyleSheet, Dimensions, Image } from 'react-native';
-import { Header, Left, Body, Right, Button, Icon } from 'native-base';
-const diviceWidth = Dimensions.get('window').width;
+import { StyleSheet, Image } from 'react-native';
+import { Icon } from 'native-base';
+import { Header } from 'react-native-elements';
 
 export default function HeaderComponent({ event }) {
   return (
-    <Header style={styles.body}>
-      <Left></Left>
-      <Body>
+    <Header
+      backgroundColor={'#202540'}
+      leftComponent={''}
+      centerComponent={
         <Image
           style={{ height: 35, alignSelf: 'center' }}
           resizeMode='contain'
           source={require('../../assets/logo.png')}
         />
-      </Body>
-      <Right>
-        <Button transparent onPress={event}>
-          <Icon name='add' style={{ color: '#F2181C', fontSize: 35 }} />
-        </Button>
-      </Right>
-    </Header>
+      }
+      rightComponent={
+        <Icon
+          onPress={event}
+          name='add'
+          style={{ color: '#F2181C', fontSize: 35, top: 5 }}
+        />
+      }
+    />
   );
 }
-const styles = StyleSheet.create({
-  body: {
-    paddingBottom: 10,
-    backgroundColor: '#202540',
-    height: 40,
-  },
-});
+const styles = StyleSheet.create({});
