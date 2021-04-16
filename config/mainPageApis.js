c39546e7f4866a5be5ee490106b8c0ad49a89787
dhmgmhw <dhmgmhw@gmail.com>
2021-04-16 23:19:07 +0900

    docs: 메인페이지 api 주소 수정

--- config/mainPageApis.js
+++ config/mainPageApis.js
@@ -2,13 +2,13 @@ import { Alert } from "react-native";
 import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const host = "https://607434bc066e7e0017e794f9.mockapi.io";
+const host = "http://3.35.208.142"
 
 export async function getBoardList() {
   try {
     const response = await axios({
       method: "get",
-      url: host + "/boards",
+      url: host + "/api",
     });
     // console.log(response.data)
     return response.data;
@@ -22,7 +22,7 @@ export async function getLastBoard(total) {
   try {
     const response = await axios({
       method: "get",
-      url: host + "/boards/" + total,
+      url: host + "/api/boards/" + total,
     });
     // console.log(response.data)
     return response.data;
@@ -34,7 +34,7 @@ export async function getLastBoard(total) {
 
 export async function createBoard(title) {
   try {
-    await axios.post(host + "/boards", {
+    await axios.post(host + "/api/boards", {
       title: title,
     });
     // console.log(title)
@@ -43,3 +43,17 @@ export async function createBoard(title) {
     Alert.alert(error);
   }
 }
+
+// export async function getTargetBoard(board_id) {
+//   try {
+//     const response = await axios({
+//       method: "get",
+//       url: host + "/api/boards/" + board_id,
+//     });
+//     // console.log(response.data)
+//     return response.data;
+//   } catch (err) {
+//     const error = err.response.data.error || err.message;
+//     Alert.alert(error);
+//   }
+// }
