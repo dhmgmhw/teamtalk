70f5e3d084fae0f7821b705cd01ba4851c9b8f8a
dhmgmhw <dhmgmhw@gmail.com>
2021-04-21 10:32:57 +0900

    feat: 로그아웃기능

--- components/main/HeaderComponent.jsx
+++ components/main/HeaderComponent.jsx
@@ -2,12 +2,28 @@ import React from 'react';
 import { StyleSheet, Image } from 'react-native';
 import { Icon } from 'native-base';
 import { Header } from 'react-native-elements';
+import { signOut } from '../../config/LoginApis';
 
-export default function HeaderComponent({ event }) {
+export default function HeaderComponent({ navigation, event }) {
+  const doLogout = async () => {
+    console.log('Logout Pressed');
+    await signOut(navigation);
+  };
+
   return (
     <Header
       backgroundColor={'#202540'}
-      leftComponent={''}
+      leftComponent={
+        <Icon
+          onPress={doLogout}
+          name='log-out-outline'
+          style={{
+            color: '#F2181C',
+            fontSize: 35,
+            top: 5,
+          }}
+        />
+      }
       centerComponent={
         <Image
           style={{ height: 35, alignSelf: 'center' }}
